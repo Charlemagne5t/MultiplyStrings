08a5a5f06ced073bce7163d3aed5ec37e1a43ef9
alek <wait82985@gmail.com>
2023-06-27 14:19:26 +0600

    initial commit. 2 test cases

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void multiplyTest1() {
+        String num1 = "2";
+        String num2 = "3";
+        String expected = "6";
+        String actual = new Solution().multiply(num1, num2);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void multiplyTest2() {
+        String num1 = "123";
+        String num2 = "456";
+        String expected = "56088";
+        String actual = new Solution().multiply(num1, num2);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+}
